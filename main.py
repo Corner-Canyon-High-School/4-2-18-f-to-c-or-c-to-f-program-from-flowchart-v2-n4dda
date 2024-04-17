3aa93f3b4a3cde544c5aad5c2ee9a44459798bee
n4dda <162729979+n4dda@users.noreply.github.com>
2024-04-17 16:52:13 +0000

    hwdg

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-
+temperature= int(input("enter the temperature:"))
+print("temp: "+ str(temperature))
+temp_unit= input(" type 1 for fehrenheit to centigrate or type 2 for centigrate to fehrenheit: ")
+if temp_unit == 1:
+    print(when converted to centigrate:)
