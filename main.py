5c7455300c62f52b18c872ed9423e81dea52a31f
n4dda <162729979+n4dda@users.noreply.github.com>
2024-04-29 15:52:23 +0000

    cryu

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 temperature= int(input("enter the temperature:"))
 print("temp: "+ str(temperature))
-temp_unit= input(" type 1 for fehrenheit to centigrate or type 2 for centigrate to fehrenheit: ")
-if temp_unit == 1:
-    print(when converted to centigrate:)
+temp_unit= input(" type 1 for fehrenheit to centigrade or type 2 for centigrade to fehrenheit: ")
+if temp_unit == "1":
+    print("when converted to centigrade: "+ str(temperature*33.8))
+
+elif temp_unit =="2":
+    print("when converted to fahrenheit: "+ str(temperature/33.8))
+else:
+    print("invalid input")
+
